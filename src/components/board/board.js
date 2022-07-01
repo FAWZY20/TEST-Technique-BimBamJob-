94deb255735f221fab8ecc57685d01c02faff502
Fawzy20 <fawzy-elsam@outlook.fr>
2022-07-01 14:19:34 +0200

    ajoute du formulaire

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,9 +1,11 @@
 import './board.css'
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
 
 
 function Board() {
-    const horizontal = [0,1,2,3,4];
-    const vertical = [0,1,2,3,4];
+    const horizontal = [0, 1, 2, 3, 4];
+    const vertical = [0, 1, 2, 3, 4];
 
     let board = []
     horizontal.forEach((i) => {
@@ -11,10 +13,23 @@ function Board() {
             board.push(<div className='tile' ></div>)
         });
     });
-    
-    return(
+
+    return (
         <div>
-             <div id='board'>{board}</div>
+            <div id='board'>
+                {board}
+            </div>
+            <div className='form' >
+                <Form>
+                    <Form.Group controlId="formFile" className="mb-3">
+                        <Form.Label>Programme de la tondeuse (format .txt) : </Form.Label>
+                        <Form.Control type="file" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Envoyer
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
 }
